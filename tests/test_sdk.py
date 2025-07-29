69aa33cbf52a97b4b7ceaac5df97318be9253ea8
ivasylenko <ivasylen@akamai.com>
2025-07-29 22:23:20 +0300

    add init

---
+++ tests/test_sdk.py
@@ -0,0 +1,6 @@
+
+
+from centra_sdk.models.connector.v1.consumer.enforcement import Action
+from centra_sdk.models.connector.v1.operations.health import ComponentFlags
+from centra_sdk.app import app
+print(Action.SET.name)
